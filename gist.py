9b65aed383fbf304c3f1d774cd2f31390920a063
Carson Evans <cevans@opentext.com>
2016-12-14 11:33:06 -0500

    Added a flag to mark gist as private

--- gist.py
+++ gist.py
@@ -1,7 +1,61 @@
 #!/usr/bin/env python
 
+"""
+Copyright (c) 2016 Carson Evans
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+of the Software, and to permit persons to whom the Software is furnished to do
+so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+IN THE SOFTWARE.
+
+--------------------------------------------------------------------------------
+
+README
+
+Three environment variables are required for this script to work
+
+GH_API_URL:
+	The url to the github api, example: https://api.github.com.  Or your
+	on-prem github, example: https://github.example.com/api
+
+GH_USERNAME:
+	Your username on github or your on-prem github.
+
+GH_GIST_TOKEN:
+	A personal access token you generated on your account.  This token should
+	have write access to gists.
+
+This script reads input from stdin.  Easiest way to get the contents of a file
+is to pipe from cat, example: cat some-text.txt | gist.py
+
+You may provide a file name and/or description for the gist with 2 optional
+parameters. These default the a timestamp.
+
+-n, --name:
+	The name of the gist
+
+-d, --description
+	The description of the gist
+
+-p, --private
+	A flag to indicate the gist should be private
+
+"""
+
 from os import getenv
-from os import environ
 from sys import stdin
 from optparse import OptionParser
 from datetime import datetime
@@ -13,6 +67,7 @@ import json
 parser = OptionParser()
 parser.add_option("-n", "--name", dest="filename", help="name of the gist")
 parser.add_option("-d", "--description", dest="description", help="description of the gist")
+parser.add_option("-p", "--private", action="store_true", dest="private", help="flag the gist as private")
 
 (options, args) = parser.parse_args()
 
@@ -37,7 +92,7 @@ for line in stdin:
 
 data = json.dumps({
 	"description": description,
-	"public": True,
+	"public": options.private is not True,
 	"files": {
 		filename: {
 			"content": TEXT
